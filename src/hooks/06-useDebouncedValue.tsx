c37f892b0282732433791030a8bda62c80e059a8
alvesronald <alvesronald@outlook.com>
2022-03-19 17:59:44 -0300

    useDebouncedValue

---
+++ src/hooks/06-useDebouncedValue.tsx
@@ -0,0 +1,21 @@
+
+import { useState, useEffect } from 'react';
+
+
+type debouncedValueType = string | number;
+
+
+export const useDebouncedValue = (value: debouncedValueType, delay: 500) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+
+        const handler = setTimeout(() => setDebouncedValue(value), delay)
+
+        return () => clearTimeout(handler)
+
+
+    }, [delay, value])
+
+    return debouncedValue;
+}
